a36fea9c10498d5d7042e0549b988494050b583b
Svetoslav Savov <speednet@mail.bg>
2022-02-06 17:45:32 +0200

    Add files via upload

---
+++ Exams/28 June 2020/01.Bomb/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bomb
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> bombs = new Dictionary<string, int>();
+            bombs.Add("Datura Bombs", 0);
+            bombs.Add("Cherry Bombs", 0);
+            bombs.Add("Smoke Decoy Bombs", 0);
+
+            Queue<int> bombEfects = new Queue<int>(Console.ReadLine()!.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Stack<int> bombCasing = new Stack<int>(Console.ReadLine()!
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            while (bombEfects.Count > 0 && bombCasing.Count > 0)
+            {
+                int bombEfect = bombEfects.Peek();
+                int bombCast = bombCasing.Peek();
+                int sum = bombEfect + bombCast;
+                if (sum == 40 || sum == 60 || sum == 120)
+                {
+                    switch (sum)
+                    {
+                        case 40:
+                            bombs["Datura Bombs"]++;
+                            bombEfects.Dequeue();
+                            bombCasing.Pop(); break;
+                        case 60:
+                            bombs["Cherry Bombs"]++;
+                            bombEfects.Dequeue();
+                            bombCasing.Pop(); break;
+                        case 120:
+                            bombs["Smoke Decoy Bombs"]++;
+                            bombEfects.Dequeue();
+                            bombCasing.Pop(); break;
+                    }
+                }
+                else
+                {
+                    bombCasing = newCasting(bombCasing);
+                }
+
+                if (bombs["Datura Bombs"] >= 3 && bombs["Cherry Bombs"] >= 3 && bombs["Smoke Decoy Bombs"] >= 3)
+                {
+                    Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+                    PrintData(bombEfects, bombCasing, bombs);
+                    Environment.Exit(0);
+                }
+            }
+
+            Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            PrintData(bombEfects, bombCasing, bombs);
+
+        }
+
+        private static Stack<int> newCasting(Stack<int> bombCasing)
+        {
+            int count = bombCasing.Count;
+            Stack<int> newCasting = new Stack<int>();
+            newCasting.Push(bombCasing.Pop() - 5);
+            for (int i = 0; i < count - 1; i++)
+            {
+                newCasting.Push(bombCasing.Pop());
+            }
+
+            bombCasing = newCasting;
+            newCasting = new Stack<int>();
+            for (int i = 0; i < count; i++)
+            {
+                newCasting.Push(bombCasing.Pop());
+            }
+            return newCasting;
+        }
+
+        private static void PrintData(Queue<int> bombEfects, Stack<int> bombCasing, Dictionary<string, int> bombs)
+        {
+            if (bombEfects.Count > 0)
+            {
+                Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEfects)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+
+            if (bombCasing.Count > 0)
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasing)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+
+            Console.WriteLine($"Cherry Bombs: {bombs["Cherry Bombs"]}");
+            Console.WriteLine($"Datura Bombs: {bombs["Datura Bombs"]}");
+            Console.WriteLine($"Smoke Decoy Bombs: {bombs["Smoke Decoy Bombs"]}");
+        }
+    }
+}
---
+++ Exams/28 June 2020/02. Snake/Program.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] snakeField = new char[size,size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string line = Console.ReadLine();
+                for (int col = 0; col < size; col++)
+                {
+                    snakeField[row, col] = line[col];
+                }
+            }
+
+            int snakeRow = GetRow(snakeField, 'S');
+            int snakeCol = GetCol(snakeField, 'S');
+
+            int countCommand = 0;
+            int food = 0;
+            while (food < 10)
+            {
+                if (IfExist(snakeField))
+                {
+                    string command = Console.ReadLine();
+                    countCommand++;
+                    snakeRow = GetRow(snakeField, 'S');
+                    snakeCol = GetCol(snakeField, 'S');
+                    if (CheckIfSpecial(snakeField, snakeRow, snakeCol, '*', command))
+                    {
+                        food++;
+                        MoveSnake(snakeField, snakeRow, snakeCol, command);
+                    }
+
+                    else if (CheckIfSpecial(snakeField, snakeRow, snakeCol, 'B', command))
+                    {
+                        MoveSnake(snakeField, snakeRow, snakeCol, command);
+                        snakeRow = GetRow(snakeField, 'S');
+                        snakeCol = GetCol(snakeField, 'S');
+
+                        snakeField[snakeRow, snakeCol] = '.';
+                        snakeRow = GetRow(snakeField, 'B');
+                        snakeCol = GetCol(snakeField, 'B');
+                        snakeField[snakeRow, snakeCol] = 'S';
+                    }
+                    else if (CheckIfIsMatrix(snakeField, snakeRow, snakeCol))
+                    {
+                        MoveSnake(snakeField, snakeRow, snakeCol, command);
+                        snakeRow = GetRow(snakeField, 'S');
+                        snakeCol = GetCol(snakeField, 'S');
+                    }
+                    else
+                    {
+                        Console.WriteLine("Game over!");
+                        Console.WriteLine($"Food eaten: {food}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Game over!");
+                    Console.WriteLine($"Food eaten: {food}");
+                    PrintField(snakeField);
+                    return;
+                }
+            }
+
+            Console.WriteLine("You won! You fed the snake.");
+            Console.WriteLine($"Food eaten: {food}");
+            PrintField(snakeField);
+        }
+
+        private static bool IfExist(char[,] snakeField)
+        {
+            foreach (var item in snakeField)
+            {
+                if (item == 'S')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void MoveSnake(char[,] snakeField, int snakeRow, int snakeCol , string command)
+        {
+            snakeField[snakeRow, snakeCol] = '.';
+            switch (command)
+            {
+                case "left":
+                    if (CheckIfIsMatrix(snakeField,snakeRow,snakeCol -1))
+                    {
+                        snakeField[snakeRow, snakeCol - 1] = 'S';
+                    }
+                    else
+                    {
+                        snakeField[snakeRow, snakeCol] = '.';
+                    }
+                    break;
+                case "right":
+                    if (CheckIfIsMatrix(snakeField, snakeRow, snakeCol + 1))
+                    {
+                        snakeField[snakeRow, snakeCol + 1] = 'S';
+                    }
+                    else
+                    {
+                        snakeField[snakeRow, snakeCol] = '.';
+                    } break;
+                case "up":
+                    if (CheckIfIsMatrix(snakeField, snakeRow -1, snakeCol))
+                    {
+                        snakeField[snakeRow - 1, snakeCol] = 'S';
+                    }
+                    else
+                    {
+                        snakeField[snakeRow, snakeCol] = '.';
+                    } break;
+                case "down":
+                    if (CheckIfIsMatrix(snakeField, snakeRow +1 , snakeCol))
+                    {
+                        snakeField[snakeRow + 1, snakeCol ] = 'S';
+                    }
+                    else
+                    {
+                        snakeField[snakeRow, snakeCol] = '.';
+                    }
+                    break;
+            }
+        }
+
+        private static bool CheckIfSpecial(char[,]snakeField, int snakeRow, int snakeCol, char item, string command)
+        {
+            switch (command)
+            {
+                case "left":
+                    if (CheckIfIsMatrix(snakeField,snakeRow,snakeCol -1))
+                    {
+                        if (snakeField[snakeRow, snakeCol - 1] == item)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+                case "right":
+                    if (CheckIfIsMatrix(snakeField, snakeRow, snakeCol + 1))
+                    {
+                        if (snakeField[snakeRow, snakeCol + 1] == item)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+                case "up":
+                    if (CheckIfIsMatrix(snakeField, snakeRow -1, snakeCol))
+                    {
+                        if (snakeField[snakeRow - 1, snakeCol] == item)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+                case "down":
+                    if (CheckIfIsMatrix(snakeField, snakeRow + 1, snakeCol))
+                    {
+                        if (snakeField[snakeRow + 1, snakeCol] == item)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+            }
+            
+
+            return false;
+        }
+
+        private static bool CheckIfIsMatrix(char[,]snakeField, int snakeRow, int snakeCol)
+        {
+            if (snakeRow >= 0 &&snakeRow < snakeField.GetLength(0) && snakeCol >= 0 && snakeCol < snakeField.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static int GetRow(char[,] snakeField, char item)
+        {
+            int snakeRow = 0;
+            for (int row = 0; row < snakeField.GetLength(0); row++)
+            {
+                for (int col = 0; col < snakeField.GetLength(1); col++)
+                {
+                    if (snakeField[row,col] == item)
+                    {
+                        snakeRow = row;
+                        break;
+                    }
+                }
+                
+            }
+            return snakeRow;
+        }
+        public static int GetCol(char[,] snakeField, char item)
+        {
+            int snakeCol = 0;
+            for (int row = 0; row < snakeField.GetLength(0); row++)
+            {
+                for (int col = 0; col < snakeField.GetLength(1); col++)
+                {
+                    if (snakeField[row, col] == item)
+                    {
+                        snakeCol = col;
+                        break;
+                    }
+                }
+
+            }
+
+            return snakeCol;
+        }
+
+        public static void PrintField(char[,] snakefield)
+        {
+            for (int i = 0; i < snakefield.GetLength(0); i++)
+            {
+                for (int j = 0; j < snakefield.GetLength(1); j++)
+                {
+                    Console.Write(snakefield[i,j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
