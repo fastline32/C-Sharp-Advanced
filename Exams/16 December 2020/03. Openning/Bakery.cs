3d4d018f862bdd7297a36ff1745743e293f5ce14
Svetoslav Savov <speednet@mail.bg>
2022-02-11 16:45:18 +0200

    Add files via upload

---
+++ Exams/16 December 2020/03. Openning/Bakery.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> employees;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            employees = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Employee> Employees { get { return this.employees;} set { this.employees = value; } }
+
+        public int Count => Employees.Count;
+
+        public void Add(Employee employee)
+        {
+            if (Employees.Count < Capacity)
+            {
+                Employees.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Employee person = Employees.Where(x => x.Name == name).FirstOrDefault();
+            if (person != null)
+            {
+                Employees.Remove(person);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Employee GetOldestEmployee() => Employees.OrderByDescending(x => x.Age).FirstOrDefault();
+
+        public Employee GetEmployee(string name) => Employees.Where(x => x.Name == name).FirstOrDefault();
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+            foreach (var person in Employees)
+            {
+                sb.AppendLine(person.ToString());
+            }
+
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exams/16 December 2020/03. Openning/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public Employee(string name,int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Employee: {Name}, {Age} ({Country})");
+
+            return sb.ToString();
+        }
+    }
+}
